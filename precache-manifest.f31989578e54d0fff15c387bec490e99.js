2faf205687ddff4a6297fee5fc33d5113e95c21d
Sakar Pathak <sakarpathak45@gmail.com>
2020-08-03 16:48:41 +0545

    Updates

---
+++ precache-manifest.f31989578e54d0fff15c387bec490e99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8111b5a7f4f85502302d97db5d2735e",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "a2b2c3179779399f347d",
+    "url": "/covid-19-tracker/static/css/main.f654a13c.chunk.css"
+  },
+  {
+    "revision": "8ddd4418f3cf62cb3431",
+    "url": "/covid-19-tracker/static/js/2.bae82d5f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.bae82d5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b2c3179779399f347d",
+    "url": "/covid-19-tracker/static/js/main.56efa266.chunk.js"
+  },
+  {
+    "revision": "8cc9c79277bcd61435dc",
+    "url": "/covid-19-tracker/static/js/runtime-main.85071d7b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/covid-19.d7265326.png"
+  }
+]);
